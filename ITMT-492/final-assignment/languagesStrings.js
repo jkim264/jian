036433fb783f77cc3cba441d280c92ce35e54a4f
jkim264 <59948918+jkim264@users.noreply.github.com>
2020-05-13 03:30:45 +0900

    Create languagesStrings.js

---
+++ ITMT-492/final-assignment/languagesStrings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  translation: {
+    SKILL_NAME: 'Simon Game',
+    EXIT_MESSAGE: 'Thanks for playing!',
+    FALLBACK_MESSAGE_DURING_GAME: `I can't help you with that, Try guessing colors among red, green, blue and yellow`,
+    FALLBACK_REPROMPT_DURING_GAME: 'Please try guessing colors among red, green, blue and yellow',
+    FALLBACK_MESSAGE_OUTSIDE_GAME: `I can't help you with that, Try guessing colors among red, green, blue and yellow. \nWould you like to play again?`,
+    GUESS_CORRECT_MESSAGE: '%s is correct! Score is %s, Would you like to play again?',
+    GUESS_WRONG_MESSAGE: "You lose. The answer is %s, Your answer is %s.\n Score is %s, Try again.",
+    LAUNCH_MESSAGE: 'Welcome to Simon Game. Would you like to play?',
+    CONTINUE_MESSAGE: 'Say yes to play or no to quit.',
+    HELP_MESSAGE: 'Sorry, an error occurred. To restart say \'quit\', and start at first',
+    HELP_REPROMPT: 'Try say correct colors',
+    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    YES_MESSAGE: 'Let\'s Start! %s'
+  }
+};
